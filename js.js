39eb8919325692e3a7657d987507d91d99ccbf34
Abhinand03 <abhinandpk0123@gmail.com>
2024-01-30 16:31:36 +0530

    second

---
+++ js.js
@@ -0,0 +1,63 @@
+
+//nav color change while scrolling 
+
+function colorChange()
+{
+    nav=document.getElementById('na-main')
+    scrollv=window.scrollY
+    if(scrollv<10)
+    {
+        nav.classList.remove('bg-change')
+    }
+    else{
+        nav.classList.add('bg-change')
+    }
+}
+window.addEventListener('scroll',colorChange)
+//------------------**********----------------------
+
+//contents chnage funtion for intro
+
+var contentIndex = 0;
+var contents = ["Front-end developer", "Designer", "Back-end developer"];
+var slidingElement = document.getElementById("slidingElement");
+
+function changeContent() {
+  // Fade out the content
+  document.getElementById("content").style.opacity = "0";
+
+  // Wait for the fade-out transition to complete
+  setTimeout(function () {
+    var element = document.getElementById("content");
+
+    // Change the content
+    element.innerHTML = contents[contentIndex];
+
+    // Fade in the new content
+    element.style.opacity = "1";
+    
+    // Reset the height
+    slidingElement.style.height = slidingElement.scrollHeight + "px";
+  }, 500); // Adjust the time according to the transition duration
+
+  // Increment the content index
+  contentIndex = (contentIndex + 1) % contents.length;
+}
+
+// Automatically change content every 3 seconds (adjust the interval as needed)
+setInterval(changeContent, 2000);
+
+
+
+
+ScrollReveal({
+
+  reset: false,
+  distance:'60px',
+  duration:2500,
+  delay:400
+})
+ScrollReveal().reveal('.intro', { delay: 500, origin: 'left', enter: 'bottom' });
+ScrollReveal().reveal('.profile-div', { delay: 500, origin: 'right', enter: 'bottom' });
+ScrollReveal().reveal('.ab-img', { delay: 500, origin: 'bottom', enter: 'bottom' });
+
